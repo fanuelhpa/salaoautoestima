4f29d0047fd3d14aa4e92483a79146b8a532f69f
Fanuel Pereira <fanuelrique@gmail.com>
2022-07-07 13:42:24 -0300

    Teste da primeira classe de dominio

--- src/main/java/com/fandev/salaoautoestima/resources/EstadoResource.java
+++ src/main/java/com/fandev/salaoautoestima/resources/EstadoResource.java
@@ -1,15 +1,29 @@
 package com.fandev.salaoautoestima.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fandev.salaoautoestima.domain.Estado;
 
 @RestController
 @RequestMapping(value="/estados")
 public class EstadoResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Estado> listar() {
+		
+		Estado estado1 = new Estado(1, "Minas Gerais");
+		Estado estado2 = new Estado(2, "Bahia");
+		
+		List<Estado> lista = new ArrayList<>();
+		lista.add(estado1);
+		lista.add(estado2);
+		
+		return lista;
+		
 	}	
 }
