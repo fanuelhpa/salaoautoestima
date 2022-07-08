8b4d778865e23d77b7dfd1f67c7e56e1737c6cb6
Fanuel Pereira <fanuelrique@gmail.com>
2022-07-08 14:13:36 -0300

    Criando operacao de instanciacao

--- src/main/java/com/fandev/salaoautoestima/SalaoautoestimaApplication.java
+++ src/main/java/com/fandev/salaoautoestima/SalaoautoestimaApplication.java
@@ -1,13 +1,33 @@
 package com.fandev.salaoautoestima;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SalaoautoestimaApplication {
+import com.fandev.salaoautoestima.domain.Estado;
+import com.fandev.salaoautoestima.repositories.EstadoRepository;
 
+@SpringBootApplication
+public class SalaoautoestimaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private EstadoRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SalaoautoestimaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Estado e1 = new Estado(null,"Minas Gerais");
+		Estado e2 = new Estado(null,"Bahia");
+		
+		repo.saveAll(Arrays.asList(e1, e2)); //Adiciona os objetos em uma lista definida no Arrays.asList e salva todos os objetos de uma vez no banco de dados
+		
 	}
 
 }
--- src/main/java/com/fandev/salaoautoestima/resources/EstadoResource.java
+++ src/main/java/com/fandev/salaoautoestima/resources/EstadoResource.java
@@ -15,15 +15,17 @@ import com.fandev.salaoautoestima.services.EstadoService;
 //O serviço por sua vez acesso o repository, que devolve o que deve ser 
 //enviado para a aplicação por este objeto.
 
-@RestController
-@RequestMapping(value="/estados")
+@RestController //anotação que define a classe como controlador Rest
+@RequestMapping(value="/estados") //anotação que mapeia o valor "estados" para o controlador Rest. Este valor será utilizado pela aplicação cliente.
 public class EstadoResource {
 	
+	//anotação que cria automaticamente uma instancia para "service", dependencia de EstadoService
 	@Autowired
 	private EstadoService service;
 
+	//anotação que define que o metodo buscar() será acessado com a especificação de um id. Ao ser passado o id, o programa programa executa o metodo buscar, recebendo o id informado.
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+	public ResponseEntity<?> buscar(@PathVariable Integer id) { //PathVariable indica que o id passado na url será atribuido ao id argumento do método
 		
 		Estado obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);	
